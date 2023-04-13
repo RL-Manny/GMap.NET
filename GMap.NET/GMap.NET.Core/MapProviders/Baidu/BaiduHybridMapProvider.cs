43c952d9a2fa7e24de498d67b3ce39d019d8e28b
Manpreet Dhesi <manpreet.dhesi@racelogic.co.uk>
2023-04-13 16:27:35 +0100

    Tidied up code for Baidu Map Providers & added net7.0-windows as a target framework

--- GMap.NET/GMap.NET.Core/MapProviders/Baidu/BaiduHybridMapProvider.cs
+++ GMap.NET/GMap.NET.Core/MapProviders/Baidu/BaiduHybridMapProvider.cs
@@ -1,46 +1,90 @@
 ﻿using System;
-using GMap.NET;
-using GMap.NET.MapProviders;
+using System.Text.RegularExpressions;
 
 namespace GMap.NET.MapProviders
 {
+    /// <summary>
+    ///     BaiduHybridMapProvider provider
+    /// </summary>
     public class BaiduHybridMapProvider : BaiduMapProviderBase
     {
-        // Fields
-        private readonly string cnName;
-        public static readonly string HybridUrlFormat;
-        private readonly Guid id = new Guid("AF522C29-9F94-4E9B-BDB3-346CD058AE7B");
         public static readonly BaiduHybridMapProvider Instance;
-        private readonly string name;
-        private GMapProvider[] overlays;
-        public string Version;
 
-        // Methods
+        private static readonly string UrlFormat;
+
+        BaiduHybridMapProvider()
+        {
+        }
+
         static BaiduHybridMapProvider()
         {
-            HybridUrlFormat = "http://online{0}.map.bdimg.com/tile/?qt=tile&x={1}&y={2}&z={3}&styles=sl&v={4}&udt=20140314";
+            UrlFormat = "http://online{0}.map.bdimg.com/tile/?qt=tile&x={1}&y={2}&z={3}&styles=sl&v={4}&udt=20140314";
             Instance = new BaiduHybridMapProvider();
         }
 
-        private BaiduHybridMapProvider()
+        #region GMapProvider Members
+
+        public override Guid Id
         {
-            this.Version = "039";
-            this.name = "BaiduHybridMap";
-            this.cnName = "百度混合地图";
+            get;
+        } = new Guid("AF522C29-9F94-4E9B-BDB3-346CD058AE7B");
+
+        public override string Name
+        {
+            get;
+        } = "BaiduHybridMap";
+
+        public override string CnName
+        {
+            get;
+        } = "百度混合地图";
+
+        GMapProvider[] _overlays;
+
+        public override GMapProvider[] Overlays
+        {
+            get
+            {
+                if (_overlays == null)
+                {
+                    _overlays = new GMapProvider[] { BaiduSatelliteMapProvider.Instance, this };
+                }
+                return _overlays;
+            }
         }
 
         public override PureImage GetTileImage(GPoint pos, int zoom)
         {
-            string url = this.MakeTileImageUrl(pos, zoom, GMapProvider.LanguageStr);
-            return base.GetTileImageUsingHttp(url);
+            string url = MakeTileImageUrl(pos, zoom, LanguageStr);
+            return GetTileImageUsingHttp(url);
+        }
+
+        #endregion
+
+        protected override string GetVersion(string contentUsingHttp)
+        {
+            Regex regex = new Regex("{\"version\":\"(\\d*)\",\"updateDate\":\".{6,8}\"},\"dem\":", RegexOptions.IgnoreCase);
+            Match match = regex.Match(contentUsingHttp);
+            if (match.Success)
+            {
+                GroupCollection groups = match.Groups;
+                if (groups.Count > 0)
+                {
+                    return groups[1].Value;
+                }
+            }
+
+            return "039";
         }
 
         private string MakeTileImageUrl(GPoint pos, int zoom, string language)
         {
-            long num = pos.X - ((long)Math.Pow(2.0, (double)(zoom - 1)));
-            long num2 = (((long)Math.Pow(2.0, (double)(zoom - 1))) - pos.Y) - 1;
+            long num = pos.X - ((long)Math.Pow(2.0, zoom - 1));
+            long num2 = ((long)Math.Pow(2.0, zoom - 1)) - pos.Y - 1;
+
             string str = num.ToString();
             string str2 = num2.ToString();
+
             if (str.StartsWith("-"))
             {
                 str = "M" + str.Substring(1);
@@ -49,44 +93,8 @@ namespace GMap.NET.MapProviders
             {
                 str2 = "M" + str2.Substring(1);
             }
-            return string.Format(HybridUrlFormat, new object[] { GMapProvider.GetServerNum(pos, BaiduMapProviderBase.maxServer) + 1, str, str2, zoom, this.Version });
-        }
 
-        // Properties
-        public string CnName
-        {
-            get
-            {
-                return this.cnName;
-            }
-        }
-
-        public override Guid Id
-        {
-            get
-            {
-                return this.id;
-            }
-        }
-
-        public override string Name
-        {
-            get
-            {
-                return this.name;
-            }
-        }
-
-        public override GMapProvider[] Overlays
-        {
-            get
-            {
-                if (this.overlays == null)
-                {
-                    this.overlays = new GMapProvider[] { BaiduSatelliteMapProvider.Instance, this };
-                }
-                return this.overlays;
-            }
+            return string.Format(UrlFormat, new object[] { GetServerNum(pos, MaxServer) + 1, str, str2, zoom, this.Version });
         }
     }
 }
--- GMap.NET/GMap.NET.Core/MapProviders/Baidu/BaiduSatelliteMapProvider.cs
+++ GMap.NET/GMap.NET.Core/MapProviders/Baidu/BaiduSatelliteMapProvider.cs
@@ -1,52 +1,81 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using GMap.NET;
-using GMap.NET.MapProviders;
+using System.Text.RegularExpressions;
 
 namespace GMap.NET.MapProviders
 {
+    /// <summary>
+    ///     BaiduSatelliteMapProvider provider
+    /// </summary>
     public class BaiduSatelliteMapProvider : BaiduMapProviderBase
     {
-        // Fields
-        private readonly string cnName;
-        public string fm;
-        private readonly Guid id = new Guid("EA7925B1-29A0-45A8-BD3C-5CC96C2ACCA4");
         public static readonly BaiduSatelliteMapProvider Instance;
-        private readonly string name;
-        public string type;
-        public static readonly string UrlFormat;
-        public string Version;
 
-        // Methods
+        private static readonly string UrlFormat;
+
+        private readonly string _fm;
+        private readonly string _type;
+
+        BaiduSatelliteMapProvider()
+        {
+            _type = "sate";
+            _fm = "46";
+        }
+
         static BaiduSatelliteMapProvider()
         {
             UrlFormat = "http://shangetu{0}.map.bdimg.com/it/u=x={1};y={2};z={3};v={4};type={5}&fm={6}&udt=20140929";
             Instance = new BaiduSatelliteMapProvider();
         }
 
-        private BaiduSatelliteMapProvider()
+        #region GMapProvider Members
+
+        public override Guid Id
         {
-            this.Version = "009";
-            this.type = "sate";
-            this.fm = "46";
-            this.name = "BaiduSatelliteMap";
-            this.cnName = "百度卫星地图";
-        }
+            get;
+        } = new Guid("EA7925B1-29A0-45A8-BD3C-5CC96C2ACCA4");
+
+        public override string Name
+        {
+            get;
+        } = "BaiduSatelliteMap";
+
+        public override string CnName
+        {
+            get;
+        } = "百度卫星地图";
 
         public override PureImage GetTileImage(GPoint pos, int zoom)
         {
-            string url = this.MakeTileImageUrl(pos, zoom, GMapProvider.LanguageStr);
-            return base.GetTileImageUsingHttp(url);
+            string url = MakeTileImageUrl(pos, zoom, LanguageStr);
+            return GetTileImageUsingHttp(url);
+        }
+
+        #endregion
+
+        protected override string GetVersion(string contentUsingHttp)
+        {
+            Regex regex = new Regex("{\"version\":\"(\\d*)\",\"updateDate\":\".{6,8}\"},\"normalTraffic\":", RegexOptions.IgnoreCase);
+            Match match = regex.Match(contentUsingHttp);
+            if (match.Success)
+            {
+                GroupCollection groups2 = match.Groups;
+                if (groups2.Count > 0)
+                {
+                    return groups2[1].Value;
+                }
+            }
+
+            return "009";
         }
 
         private string MakeTileImageUrl(GPoint pos, int zoom, string language)
         {
-            long num = pos.X - ((long)Math.Pow(2.0, (double)(zoom - 1)));
-            long num2 = (((long)Math.Pow(2.0, (double)(zoom - 1))) - pos.Y) - 1;
+            long num = pos.X - ((long)Math.Pow(2.0, zoom - 1));
+            long num2 = ((long)Math.Pow(2.0, zoom - 1)) - pos.Y - 1;
+
             string str = num.ToString();
             string str2 = num2.ToString();
+
             if (str.StartsWith("-"))
             {
                 str = "M" + str.Substring(1);
@@ -55,32 +84,8 @@ namespace GMap.NET.MapProviders
             {
                 str2 = "M" + str2.Substring(1);
             }
-            return string.Format(UrlFormat, new object[] { GMapProvider.GetServerNum(pos, BaiduMapProviderBase.maxServer) + 1, str, str2, zoom, this.Version, this.type, this.fm });
-        }
 
-        // Properties
-        public string CnName
-        {
-            get
-            {
-                return this.cnName;
-            }
-        }
-
-        public override Guid Id
-        {
-            get
-            {
-                return this.id;
-            }
-        }
-
-        public override string Name
-        {
-            get
-            {
-                return this.name;
-            }
+            return string.Format(UrlFormat, new object[] { GetServerNum(pos, MaxServer) + 1, str, str2, zoom, Version, _type, _fm });
         }
     }
 }
